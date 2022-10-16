44a41bfbdc2298105c093f3819268b76df8e3c17
Sachin Kumar <sachin.kumar1@daffodilsw.com>
2022-10-16 20:40:13 +0530

    fixed

--- src/utils/Modal.js
+++ src/utils/Modal.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import '../style/modalStyle.css'
 
-const ReactCustomModal =(props) => {
-  const closeCustomModal=()=>{
-   props.setShowModal(false)
-  }
+const ReactCustomModal =({onCloseModal}) => {
     return (
         <div className="modal-content">
 
           <div className="modal-header">
-            <span className="close" onClick={closeCustomModal}>&times;</span>
+            <span className="close" onClick={onCloseModal}>&times;</span>
             <h4>Amount Added Successfully</h4>
           </div>
         </div>
